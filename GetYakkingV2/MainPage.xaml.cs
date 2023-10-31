69e395005bf7478a61dee0e754f4cb4f3312710f
Sacha Miller <SachaM@forrit.com>
2023-10-31 09:40:40 +0000

    update to add bubbles

--- GetYakkingV2/MainPage.xaml.cs
+++ GetYakkingV2/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.Maui.Controls;
 using System.Timers;
+using Microsoft.Maui.Graphics;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace GetYakkingV2
 {
@@ -13,6 +15,18 @@ namespace GetYakkingV2
             InitializeComponent();
             timer = new System.Timers.Timer(1000);
             timer.Start();
+        }
+        public void OnDraw(object sender, DrawEventArgs e)
+        {
+            var canvas = e.Canvas;
+            // Draw bubbles here using canvas.DrawCircle(), etc.
+            for (int i = 0; i < 10; i++)  // Create 10 bubbles
+            {
+                float x = (float)new Random().Next(0, 100);
+                float y = (float)new Random().Next(0, 100);
+                canvas.FillColor = Colors.Pink;
+                canvas.DrawCircle(x, y, 25);
+            }
         }
 
         private void OnClassicClicked(object sender, EventArgs e)
