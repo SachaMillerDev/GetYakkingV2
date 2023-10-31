e10da57060a49ba04535c89e45d8dd08f3e7d6f3
Sacha Miller <SachaM@forrit.com>
2023-10-31 10:43:53 +0000

    changes to add pages

---
+++ GetYakkingV2/CouplesPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using System.Timers;
+
+namespace GetYakkingV2
+{
+    public partial class CouplesPage : ContentPage
+    {
+        private System.Timers.Timer timer;
+
+        public CouplesPage(string message)
+        {
+            InitializeComponent();
+            ShowMessage(message);
+        }
+
+        private void ShowMessage(string message)
+        {
+            messageLabel.Text = message;
+            messageLabel.Opacity = 0;
+            messageLabel.FadeTo(1, 1000);
+            timer = new System.Timers.Timer(5000);
+            timer.Elapsed += (sender, e) => messageLabel.FadeTo(0, 1000);
+            timer.Start();
+        }
+    }
+}
--- GetYakkingV2/MainPage.xaml.cs
+++ GetYakkingV2/MainPage.xaml.cs
@@ -1,41 +1,28 @@
 ﻿using System;
 using Microsoft.Maui.Controls;
-using System.Timers;
 
 namespace GetYakkingV2
 {
     public partial class MainPage : ContentPage
     {
-        private System.Timers.Timer timer;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnClassicClicked(object sender, EventArgs e)
+        private async void OnClassicClicked(object sender, EventArgs e)
         {
-            ShowMessage("Welcome to classic GetYakking! ...");
+            await Navigation.PushAsync(new ClassicPage("Welcome to classic GetYakking! ..."));
         }
 
-        private void OnCouplesClicked(object sender, EventArgs e)
+        private async void OnCouplesClicked(object sender, EventArgs e)
         {
-            ShowMessage("Welcome to couples Yakking! ...");
+            await Navigation.PushAsync(new CouplesPage("Welcome to couples Yakking! ..."));
         }
 
-        private void OnRiskyClicked(object sender, EventArgs e)
+        private async void OnRiskyClicked(object sender, EventArgs e)
         {
-            ShowMessage("Drunk Yakking is played ...");
-        }
-
-        private void ShowMessage(string message)
-        {
-            messageLabel.Text = message;
-            messageLabel.Opacity = 0;
-            messageLabel.FadeTo(1, 1000);
-            timer = new System.Timers.Timer(5000);
-            timer.Elapsed += (sender, e) => messageLabel.FadeTo(0, 1000);
-            timer.Start();
+            await Navigation.PushAsync(new RiskyPage("Drunk Yakking is played ..."));
         }
     }
 }
