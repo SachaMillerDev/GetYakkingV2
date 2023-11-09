a2834e6ec0a6f0c0be21241fcf7d9ada4da912c0
Sacha Miller <SachaM@forrit.com>
2023-11-09 14:47:40 +0000

    update

--- GetYakkingV2/ClassicPage.xaml.cs
+++ GetYakkingV2/ClassicPage.xaml.cs
@@ -14,20 +14,28 @@ namespace GetYakkingV2
         {
             if (frontView.IsVisible)
             {
-                await frontView.RotateYTo(90, 250); // Rotate halfway
+                await frontView.RotateYTo(-90, 250); // Rotate halfway
                 frontView.IsVisible = false;
                 backView.IsVisible = true;
-                backView.RotationY = 270; // Set rotation to 270 degrees to prepare for flip back
-                await backView.RotateYTo(360, 250); // Rotate the rest of the way
+                backView.RotationY = -270; // Set rotation to -270 degrees to prepare for flip back
+                await backView.RotateYTo(-360, 250); // Rotate the rest of the way
             }
             else
             {
-                await backView.RotateYTo(270, 250); // Rotate halfway
+                await backView.RotateYTo(-270, 250); // Rotate halfway
                 backView.IsVisible = false;
                 frontView.IsVisible = true;
-                frontView.RotationY = 90; // Set rotation to 90 degrees to prepare for flip back
+                frontView.RotationY = -90; // Set rotation to -90 degrees to prepare for flip back
                 await frontView.RotateYTo(0, 250); // Rotate the rest of the way
             }
         }
+
+        private void OnRulesClicked(object sender, EventArgs e)
+        {
+            // Hide the card and show the rules
+            card.IsVisible = false;
+            rulesLabel.IsVisible = true;
+        }
+
     }
 }
